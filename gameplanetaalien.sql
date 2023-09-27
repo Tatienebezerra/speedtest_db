89c0999361ecfdd351a0230d18dba46984403574
ead <ead@academico.local>
2023-09-27 10:38:15 -0300

    finalizado

--- gameplanetaalien.sql
+++ gameplanetaalien.sql
@@ -10,36 +10,53 @@ email varchar(80) NOT NULL,
 senha nchar(8) NOT NULL
 );
 
-Show tables;
+/* inserindo jogador da cei*/
+INSERT INTO usuario(nome,apelido,email,senha)
+    VALUES('Romulo','jogador-da-cei','jogao@gmail.com','34567898');
 
-Describe usuario;
-
+/*inserindo mais 4 amigos*/
 INSERT INTO usuario(nome,apelido,email,senha)
     VALUES('Tatiene','Taty','tatiene@gmail.com','12345678');
 INSERT INTO usuario(nome,apelido,email,senha)
     VALUES('Celso','Celsao','celso@gmail.com','12345678');    
-   INSERT INTO usuario(nome,apelido,email,senha)
+INSERT INTO usuario(nome,apelido,email,senha)
     VALUES('Miqueias','Mlk','miqueias@gmail.com','12345678'); 
-    INSERT INTO usuario(nome,apelido,email,senha)
+INSERT INTO usuario(nome,apelido,email,senha)
     VALUES('Pedro Guilherme','PG','pedro@gmail.com','12345678');
-    INSERT INTO usuario(nome,apelido,email,senha)
-    VALUES('Artur','Tutu','artur@gmail.com','12345678');
-    INSERT INTO usuario(nome,apelido,email,senha)
-    VALUES('Flavio','Fla','flavio@gmail.com','12345678');
-    INSERT INTO usuario(nome,apelido,email,senha)
-    VALUES('Thiago','Tiaguinho','tiago@gmail.com','12345678');
-    INSERT INTO usuario(nome,apelido,email,senha)
-    VALUES('Vitoria','Vit','vitoria@gmail.com','12345678');
     
+/*deletando um amigo*/
+DELETE FROM usuario WHERE id=1;
+
+/*atualizando os dados*/
+UPDATE usuario SET nome = 'joao',apelido='joao jogador', email='joao@gmail.com' WHERE id=3;
+
+/*fazendo uma pesquisa*/
 Select *from usuario;
 
-SELECT apelido FROM usuario;
-SELECT apelido,senha FROM usuario;
-SELECT apelido,senha,email FROM usuario;
+/*inserindo diversos jogadores*/
+INSERT INTO usuario(nome,apelido,email,senha)
+    VALUES('Artur','Tutu','artur@gmail.com','12345678');
+INSERT INTO usuario(nome,apelido,email,senha)
+    VALUES('Flavio','Fla','flavio@gmail.com','12345678');
+INSERT INTO usuario(nome,apelido,email,senha)
+    VALUES('Thiago','Tiaguinho','tiago@gmail.com','12345678');
+INSERT INTO usuario(nome,apelido,email,senha)
+    VALUES('Vitoria','Vit','vitoria@gmail.com','12345678');
 
-DELETE FROM usuario WHERE id=2;
-UPDATE usuario SET nome = 'Ambrosina_Games'WHERE id=3;
-UPDATE usuario SET apelido = 'Brosina'WHERE id=3;
-UPDATE usuario SET email = 'ambrosina@gmail.com'WHERE id=3;
+/*excluindo usuarios com uma condicao*/ 
+DELETE FROM usuario WHERE id=9;
 
-DELETE FROM usuario WHERE id=null;
+/*excluindo usuarios de um intervalo*/
+DELETE FROM usuario WHERE id>4 and id<= 10;
+
+
+Show tables;
+
+Describe usuario;
+
+
+
+
+
+    
+    
